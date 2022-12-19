08e9002b8c65f5d63c103a6209372436d9610263
Jasper Sival <jasper.sival@alliander.com>
2022-12-19 16:30:25 +0100

    Create guessing game

---
+++ guessing/src/main.rs
@@ -0,0 +1,36 @@
+/*Guessing game. ask the user to guess a number between 1 and a 100. If you guessed correctly,
+it will say you win. If you're too high or too low it will also let you know.
+*/
+
+use rand::Rng;
+use std::cmp::Ordering;
+
+use std::io;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let winning_num = rng.gen_range(1..=100);
+    loop {
+        println!("Guess a number between 0 and 100");
+        let mut user_input = String::new();
+        let stdin = io::stdin();
+        stdin
+            .read_line(&mut user_input)
+            .expect("Failed to read input");
+        let number = user_input.trim().parse::<u8>().unwrap();
+
+        if number > 100 {
+            eprintln!("Error, the given value {} is greater than 100.", number);
+            continue;
+        }
+
+        match number.cmp(&winning_num) {
+            Ordering::Greater => println!("Too high!. You guessed {}.", number),
+            Ordering::Less => println!("Too low!. You guessed {}", number),
+            Ordering::Equal => {
+                println!("You have won with winning number {}", number);
+                break;
+            }
+        }
+    }
+}
